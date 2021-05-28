c43688c599d2a524238c48f2293744dd3064b96a
Owen <owen@howtank.com>
2021-05-28 23:36:50 +0200

    ajout des controlleurs de contrats & vehicules

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContratController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,6 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/contrats', function () {
+    return view('contrats');
+});
+
+Route::get('/contrat/{id}', [ContratController::class, 'show']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
